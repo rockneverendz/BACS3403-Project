9089cb1c0fa38c32a4b1b7bc0f186f22902df0bf
rockneverendz <31733763+rockneverendz@users.noreply.github.com>
2020-07-20 09:46:21 +0800

    Add Register, Login, LogOut, and RegisterConfirmation
    Thru scaffold

---
+++ Areas/Identity/IdentityHostingStartup.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using BACS3403_Project.Data;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Identity.UI;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+
+[assembly: HostingStartup(typeof(BACS3403_Project.Areas.Identity.IdentityHostingStartup))]
+namespace BACS3403_Project.Areas.Identity
+{
+    public class IdentityHostingStartup : IHostingStartup
+    {
+        public void Configure(IWebHostBuilder builder)
+        {
+            builder.ConfigureServices((context, services) => {
+            });
+        }
+    }
+}
