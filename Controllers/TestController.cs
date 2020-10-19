79cc2cb9dc85496481f2b69ed7427f5523665fc9
rockneverendz <31733763+rockneverendz@users.noreply.github.com>
2020-10-19 21:26:26 +0800

    Add CKEditor Test
    /Test

---
+++ Controllers/TestController.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+namespace BACS3403_Project.Controllers
+{
+    public class TestController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult Create()
+        {
+            return View();
+        }
+    }
+}
