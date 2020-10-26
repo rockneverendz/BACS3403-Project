7cf855de3a11d32c74c6990495adac9c034a32ca
rockneverendz <31733763+rockneverendz@users.noreply.github.com>
2020-10-26 21:51:19 +0800

    Scaffold CandidatesController.cs

---
+++ Controllers/CandidatesController.cs
@@ -0,0 +1,110 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using BACS3403_Project.Data;
+using BACS3403_Project.Models;
+
+namespace BACS3403_Project.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class CandidatesController : ControllerBase
+    {
+        private readonly ApplicationDbContext _context;
+
+        public CandidatesController(ApplicationDbContext context)
+        {
+            _context = context;
+        }
+
+        // GET: api/Candidates
+        [HttpGet]
+        public async Task<ActionResult<IEnumerable<Candidate>>> GetCandidates()
+        {
+            return await _context.Candidates.ToListAsync();
+        }
+
+        // GET: api/Candidates/5
+        [HttpGet("{id}")]
+        public async Task<ActionResult<Candidate>> GetCandidate(int id)
+        {
+            var candidate = await _context.Candidates.FindAsync(id);
+
+            if (candidate == null)
+            {
+                return NotFound();
+            }
+
+            return candidate;
+        }
+
+        // PUT: api/Candidates/5
+        // To protect from overposting attacks, enable the specific properties you want to bind to, for
+        // more details, see https://go.microsoft.com/fwlink/?linkid=2123754.
+        [HttpPut("{id}")]
+        public async Task<IActionResult> PutCandidate(int id, Candidate candidate)
+        {
+            if (id != candidate.CandidateID)
+            {
+                return BadRequest();
+            }
+
+            _context.Entry(candidate).State = EntityState.Modified;
+
+            try
+            {
+                await _context.SaveChangesAsync();
+            }
+            catch (DbUpdateConcurrencyException)
+            {
+                if (!CandidateExists(id))
+                {
+                    return NotFound();
+                }
+                else
+                {
+                    throw;
+                }
+            }
+
+            return NoContent();
+        }
+
+        // POST: api/Candidates
+        // To protect from overposting attacks, enable the specific properties you want to bind to, for
+        // more details, see https://go.microsoft.com/fwlink/?linkid=2123754.
+        [HttpPost]
+        public async Task<ActionResult<Candidate>> PostCandidate(Candidate candidate)
+        {
+            _context.Candidates.Add(candidate);
+            await _context.SaveChangesAsync();
+
+            return CreatedAtAction("GetCandidate", new { id = candidate.CandidateID }, candidate);
+        }
+
+        // DELETE: api/Candidates/5
+        [HttpDelete("{id}")]
+        public async Task<ActionResult<Candidate>> DeleteCandidate(int id)
+        {
+            var candidate = await _context.Candidates.FindAsync(id);
+            if (candidate == null)
+            {
+                return NotFound();
+            }
+
+            _context.Candidates.Remove(candidate);
+            await _context.SaveChangesAsync();
+
+            return candidate;
+        }
+
+        private bool CandidateExists(int id)
+        {
+            return _context.Candidates.Any(e => e.CandidateID == id);
+        }
+    }
+}
