5aa61c6326d438ea7df58f5e988d397a6d7be8aa
NO NaMe <36953143+Soocyang@users.noreply.github.com>
2020-11-12 21:52:04 +0800

    Add Grades and TestAdmin CV

---
+++ Controllers/GradesController.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+namespace BACS3403_Project.Controllers
+{
+	public class GradesController : Controller
+	{
+		public IActionResult Index()
+		{
+			return View();
+		}
+
+		public IActionResult ReviewTestAnswers()
+		{
+			return View();
+		}
+
+		public IActionResult GenerateGradeReport()
+		{
+			return View();
+		}
+
+
+
+	}
+}
