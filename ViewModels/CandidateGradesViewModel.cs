dec43668c2615ec7257779186c76ca60da49e414
NO NaMe <36953143+Soocyang@users.noreply.github.com>
2020-11-19 09:50:40 +0800

    Add viewmodels for posting and getting candidates list

---
+++ ViewModels/CandidateGradesViewModel.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace BACS3403_Project.ViewModels
+{
+	public class CandidateGradesViewModel
+	{
+		public int Index { get; set; }
+		public int CandidateID { get; set; }
+		public string Status { get; set; }
+		public int TotalMarks{ get; set; }
+		public string Grade { get; set; }
+	}
+}
