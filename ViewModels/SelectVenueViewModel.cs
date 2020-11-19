2116e5df4d868633941a1cbed4e377e8eb55c72e
NO NaMe <36953143+Soocyang@users.noreply.github.com>
2020-11-19 09:50:11 +0800

    Add view models for cascading ddl

---
+++ ViewModels/SelectVenueViewModel.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace BACS3403_Project.ViewModels
+{
+	public class SelectVenueViewModel
+	{
+		public string VenueValue { get; set; }
+		public string VenueName { get; set; }
+	}
+}
