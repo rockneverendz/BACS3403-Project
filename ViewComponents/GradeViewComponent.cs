baf03a480cba573eeef3030c1844fd3e634a7e8a
NO NaMe <36953143+Soocyang@users.noreply.github.com>
2020-11-19 09:51:14 +0800

    Add CandidatesGrades ViewComponent

---
+++ ViewComponents/GradeViewComponent.cs
@@ -0,0 +1,54 @@
+﻿using BACS3403_Project.Data;
+using BACS3403_Project.ViewModels;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace BACS3403_Project.ViewComponents
+{
+	public class GradeViewComponent : ViewComponent
+	{
+		private readonly ApplicationDbContext _context;
+
+		public GradeViewComponent(ApplicationDbContext context)
+		{
+			_context = context;
+		}
+
+		public IViewComponentResult Invoke()
+		{
+			List<CandidateGradesViewModel> candidateGrades = new List<CandidateGradesViewModel> 
+			{
+				new CandidateGradesViewModel
+				{
+					Index = 1,
+					CandidateID = 1001,
+					Status = "Present",
+					TotalMarks = 90,
+					Grade = "Band 5"
+				},
+				new CandidateGradesViewModel
+				{
+					Index = 2,
+					CandidateID = 1002,
+					Status = "Present",
+					TotalMarks = 80,
+					Grade = "Band 5"
+				},
+				new CandidateGradesViewModel
+				{
+					Index = 3,
+					CandidateID = 1003,
+					Status = "Present",
+					TotalMarks = 75,
+					Grade = "Band 4"
+				}
+			};
+
+			return View(candidateGrades);
+		}
+
+	}
+}
