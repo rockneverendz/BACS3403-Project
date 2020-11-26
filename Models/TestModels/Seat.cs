3e726085f2250fb1762b15cd2a32a4380d8d5067
NO NaMe <36953143+Soocyang@users.noreply.github.com>
2020-11-26 17:23:43 +0800

    Update Seat.cs
    Candidate and TimeClaimed nullable

--- Models/TestModels/Seat.cs
+++ Models/TestModels/Seat.cs
@@ -5,10 +5,9 @@ namespace BACS3403_Project.Models
     public class Seat
     {
         public int SeatID { get; set; }
-        public string Venue { get; set; }
-        public DateTime TimeClaimed { get; set; }
-        public int CandidateID { get; set; }
-
+        public DateTime? TimeClaimed { get; set; }
+        public int? CandidateID { get; set; }
+        public int TestID { get; set; }
         public Candidate Candidates { get; set; }
         public Test Test { get; set; }    
     }
