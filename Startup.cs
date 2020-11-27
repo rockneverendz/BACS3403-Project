c9e3754a17c953942e2a4d63fe13bb1fdd5d97a3
NO NaMe <36953143+Soocyang@users.noreply.github.com>
2020-11-27 20:41:23 +0800

    Update Timeout

--- Startup.cs
+++ Startup.cs
@@ -52,7 +52,7 @@ namespace BACS3403_Project
                 options.Password.RequiredUniqueChars = 1;
 
                 // Lockout settings.
-                options.Lockout.DefaultLockoutTimeSpan = TimeSpan.FromMinutes(5);
+                options.Lockout.DefaultLockoutTimeSpan = TimeSpan.FromMinutes(15);
                 options.Lockout.MaxFailedAccessAttempts = 5;
                 options.Lockout.AllowedForNewUsers = true;
 
